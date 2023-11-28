027bab8b931dd83a51e424a98073fa58790207ae
Eric Lin <ericlinsechs@gmail.com>
2023-11-28 16:23:47 +0800

    Update blur

--- helpers.c
+++ helpers.c
@@ -1,5 +1,6 @@
 #include "helpers.h"
 #include <math.h>
+#include <string.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -42,6 +43,36 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE tmp_img[height][width];
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int sum_r = 0;
+            int sum_g = 0;
+            int sum_b = 0;
+            unsigned int cnt = 0;
+
+            for (int x = i - 1; x <= (i + 1); x++) {
+                if (x < 0 || x >= height)
+                    continue;
+                for (int y = j - 1; y <= (j + 1); y++) {
+                    if (y < 0 || y >= width)
+                        continue;
+                    sum_r += image[x][y].rgbtRed;
+                    sum_g += image[x][y].rgbtGreen;
+                    sum_b += image[x][y].rgbtBlue;
+                    cnt++;
+                }
+            }
+
+            tmp_img[i][j].rgbtRed = round((float) sum_r / cnt);
+            tmp_img[i][j].rgbtGreen = round((float) sum_g / cnt);
+            tmp_img[i][j].rgbtBlue = round((float) sum_b / cnt);
+        }
+    }
+
+    memcpy(image, tmp_img, sizeof(RGBTRIPLE) * height * width);
+
     return;
 }
 
