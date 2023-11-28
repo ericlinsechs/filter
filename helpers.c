2fb559100ee045e54c373989f0db8d459657b66b
Eric Lin <ericlinsechs@gmail.com>
2023-11-28 13:12:15 +0800

    Update grayscale

--- helpers.c
+++ helpers.c
@@ -1,8 +1,22 @@
+#include <math.h>
 #include "helpers.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Change all black pixels to a color of your choosing
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sum = image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue;
+
+            int ave = round((float)sum / 3); 
+
+            RGBTRIPLE gray_scale = {.rgbtRed = ave, .rgbtGreen = ave, .rgbtBlue = ave};
+            image[i][j] = gray_scale;
+        }
+    }
     return;
 }
 
