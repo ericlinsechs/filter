a614113159bcd979bf65244f3a32c919caf413fb
Eric Lin <ericlinsechs@gmail.com>
2023-11-28 14:39:39 +0800

    Update grayscale to clang format

--- helpers.c
+++ helpers.c
@@ -1,19 +1,19 @@
-#include <math.h>
 #include "helpers.h"
+#include <math.h>
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            int sum = image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            int sum = image[i][j].rgbtRed + image[i][j].rgbtGreen +
+                      image[i][j].rgbtBlue;
 
-            int ave = round((float)sum / 3); 
+            int ave = round((float) sum / 3);
 
-            RGBTRIPLE gray_scale = {.rgbtRed = ave, .rgbtGreen = ave, .rgbtBlue = ave};
+            RGBTRIPLE gray_scale = {
+                .rgbtRed = ave, .rgbtGreen = ave, .rgbtBlue = ave};
             image[i][j] = gray_scale;
         }
     }
